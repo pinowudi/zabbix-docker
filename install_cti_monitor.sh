56e4526841e2a16b053595499489af490aaab633
Irrational Pi <pinowudi@gmail.com>
2020-10-24 13:46:22 +0000

    updated for use on ARM devices. Wrote script to install docker service for reboot. Adjusted nginx to include libaio for some permissions reason with docker.

--- install_cti_monitor.sh
+++ install_cti_monitor.sh
@@ -12,7 +12,30 @@ sudo systemctl restart NetworkManager && \
 sudo systemctl restart dnscrypt-proxy && \
 sudo apt-get autoremove && \
 sudo docker-compose -f cti_monitor.yaml build && \
+cat << EOF > docker-compose-app.service
+# /etc/systemd/system/docker-compose-app.service
+
+[Unit]
+Description=Docker Compose Application Service
+Requires=docker.service
+After=docker.service
+
+[Service]
+Type=oneshot
+RemainAfterExit=yes
+WorkingDirectory=/srv/docker/zabbix-docker
+cat <<EOF >> docker-compose-app.service
+ExecStart=/usr/local/bin/docker-compose up -d
+ExecStop=/usr/local/bin/docker-compose down
+TimeoutStartSec=0
+
+[Install]
+WantedBy=multi-user.target
+EOF
+sudo mkdir /srv/docker && \
+sudo ln -s $(pwd) /srv/docker/zabbix-docker && \
 sudo cp docker-compose-app.service /etc/systemd/system/docker-compose-app.service && \
+sudo ln -s cti_monitor.yaml docker-compose.yaml && \
 sudo systemctl enable docker-compose-app && \
 sudo systemctl restart docker-compose-app && \
 reboot
